d9ad39ad90cefbd1b9b9299aee23c8844c92c466
himanisheik2504-crypto <himanisheik2504@gmail.com>
2026-01-19 19:18:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "eks" {
   scaling_desired_size    = 1
   scaling_max_size        = 2
   scaling_min_size        = 1
-  instance_types          = ["t3.medium"]
+  instance_types          = ["t3.micro"]
   key_pair                = "TestKeyPair"
 }
 
