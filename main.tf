6da5de7221f11d41a4cb334aa15baa11fa0e3058
himanisheik2504-crypto <himanisheik2504@gmail.com>
2026-01-19 18:05:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ module "eks" {
   endpoint_private_access = false
   public_access_cidrs     = ["0.0.0.0/0"]
   node_group_name         = "cloudquicklabs"
-  scaling_desired_size    = 2
-  scaling_max_size        = 1
+  scaling_desired_size    = 1
+  scaling_max_size        = 2
   scaling_min_size        = 1
   instance_types          = ["t3.medium"]
   key_pair                = "TestKeyPair"
